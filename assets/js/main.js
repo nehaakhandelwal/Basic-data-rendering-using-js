ac476dbd7fe2912f208f5f929e34812538eebabc
Khandelwal <neha.b.khandelwal@capgemini.com>
2020-08-11 17:12:16 +0530

    dropdown done

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+// var location = [{
+//         username: "Neha",
+//         place: "Jaipur"
+//     },
+//     {
+//         username: "Priya",
+//         place: "Jodhpur"
+//     },
+//     {
+//         username: "Megha",
+//         place: "Udaipur"
+//     },
+//     {
+//         username: "Riyu",
+//         place: "Jaipur"
+//     },
+//     {
+//         username: "Preyan",
+//         place: "Jodhpur"
+//     },
+//     {
+//         username: "Yash",
+//         place: "Udaipur"
+//     }
+// ];
+
+$('#dropdown a').on('click', function() {
+    var txt = ($(this).text());
+    alert(txt);
+});
+
+// function details(loc) {
+//     alert(loc);
+// }
